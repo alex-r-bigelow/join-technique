b25f2267743ac63fe77fb533091d80bbd20ade1c
Alex Bigelow <alex.r.bigelow@gmail.com>
2017-03-03 11:39:59 -0700

    Add tree view basics

--- views/GraphicsTreeView/index.js
+++ views/GraphicsTreeView/index.js
@@ -1,3 +1,4 @@
+import * as d3 from '../../lib/d3.min.js';
 import JoinableView from '../JoinableView';
 
 import template from './template.html';
@@ -18,12 +19,48 @@ class GraphicsTreeView extends JoinableView {
       window.setTimeout(() => { this.dirty = true; this.render(d3el); }, 200);
       return;
     }
-
-    // A flag to force an update of the visible positions
-    this.firstRender = true;
   }
   draw (d3el) {
-    // d3el.select('#graphicsContent').html(this.model.)
+    function drawTreeLevel (targetD3element, sourceParentElement, depth) {
+      let children = Array.from(sourceParentElement.children);
+      if (children.length === 0) {
+        return;
+      }
+      let details = targetD3element.selectAll('details').data(children);
+      details.exit().remove();
+      let detailsEnter = details.enter().append('details');
+      let summaryEnter = detailsEnter.append('summary');
+      summaryEnter.append('div').classed('tagName', true);
+      summaryEnter.append('div').classed('rootSelector', true);
+      detailsEnter.append('ul');
+      details = detailsEnter.merge(details);
+
+      // details.style('left', -depth + 'em');
+
+      details.select('summary')
+        .style('left', depth + 'em')
+        .style('width', 'calc(100% - ' + depth + 'em)')
+        .select('.tagName')
+        .text(d => d.tagName);
+
+      let attributes = details.select('ul').selectAll('li')
+        .data(d => Array.from(d.attributes));
+      attributes.exit().remove();
+      let attributesEnter = attributes.enter().append('li');
+      attributesEnter.append('div').classed('attributeName', true);
+      attributesEnter.append('div').classed('attributeValue', true);
+      attributes = attributesEnter.merge(attributes);
+
+      attributes.select('.attributeName').text(d => d.name);
+      attributes.select('.attributeValue').text(d => d.value);
+
+      // recursively append the next level below each child
+      details.each(function (d) {
+        // this refers to the DOM element
+        drawTreeLevel(d3.select(this), d, depth + 1);
+      });
+    }
+    drawTreeLevel(d3el.select('#graphicsTree'), this.model.doc, 0);
 
     this.updateVisibleLocations(d3el);
   }
